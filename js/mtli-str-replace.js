26f7d649b90bfc9c84e3d8c22718fac2c3d6303c
jrfnl <codebase_nospam@adviesenzo.nl>
2013-05-16 04:16:42 +0200

    Version 3.0 initial commit, for changelog, please refer to the v3.0 changelog in the readme file

---
+++ js/mtli-str-replace.js
@@ -0,0 +1,69 @@
+/* Javascript functions for plugin: MimeTypes Link Icons */
+/*
+ @todo look into issue: http://wordpress.org/support/topic/problem-with-images-13?replies=5
+
+ Let us know how you get on. We can always use a jquery wildcard selector instead of classname in a later version of the plugin, so
+
+ jQuery(this).parents('.myexcludedclassname').length)
+
+ becomes
+
+ jQuery(this).parents('[class^=wp-image-]').length /* matches all images on my theme
+ */
+
+/* Using alternative to jQuery(document).ready(function() as WP3.2.1/jQuery1.6.1 does not seem to like it
+ */
+(function(){
+
+	if(i18n_mtli.enable_async == 1){
+
+		var contentObj = jQuery('#content');
+        if (contentObj.length == 0) {
+            contentObj = jQuery('.status-publish:first').parent();
+        }
+
+		var contentLinks = contentObj.find('a');
+//console.log( '# of links found: '+content_links.length );
+
+		if(i18n_mtli.oldwp == 1){
+			i18n_mtli.mime_array = i18n_mtli.mime_array.split(',');
+		}
+//console.log( '# of mime types: '+i18n_mtli.mime_array.length );
+
+		jQuery.each(i18n_mtli.mime_array, function( i, elm ) {
+//console.log( 'Evaluating mime_type '+i+' : '+elm );
+//			content_obj.find('[href*=".'+elm+'"]').addClass('mtli_attachment mtli_'+elm); // works, but case-sensitive and .ai also matches .air
+
+			var reg = new RegExp('^([^"#]+\\.'+elm+')(#[^" ]+$|$)','i');
+//var filtered = content_links.filter(function( index ) {
+			contentLinks.filter(function(){
+				return ( jQuery(this).attr('href').search(reg) >= 0 );
+			}).addClass('mtli_attachment mtli_'+elm);
+
+//console.log( 'Evaluating mime_type '+i+' : '+elm+"\t\t\t"+'filtered links: '+filtered.length );
+		});
+	}
+
+
+	if(i18n_mtli.hidethings == 1){
+//console.log( 'nr of elements found : '+jQuery(i18n_mtli.avoid_selector).length );
+		jQuery(i18n_mtli.avoid_selector).each(function(){
+//console.log( 'testing elm '+index+' : '+elm );
+			// If the current element has the mtli_attachment class, remove it
+			if( jQuery(this).hasClass('mtli_attachment') ) {
+//console.log( "\t"+'element has class mtli_attachment' );
+				jQuery(this).removeClass('mtli_attachment').css('background-image','none');
+				if( jQuery(this).attr('rel') != undefined && jQuery(this).attr('rel').indexOf('mtli_filesize') >= 0 ) {
+//console.log( "\t\t"+'rel mtli_filesize found' );
+					jQuery(this).removeAttr('rel');
+				}
+			}
+			// If the current element has descendants with the mtli_attachment class, remove it
+//console.log( "\t"+'# of jQuery(this).find(.mtli_attachment) : '+jQuery(this).find('.mtli_attachment').length );
+//console.log( "\t"+'# of jQuery(this).find([rel^="mtli_filesize"]) : '+jQuery(this).find('[rel^="mtli_filesize"]').length );
+			jQuery(this).find('.mtli_attachment').removeClass('mtli_attachment').css('background-image','none');
+			jQuery(this).find('[rel^="mtli_filesize"]').removeAttr('rel');
+		});
+	}
+
+})(jQuery);
---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Code used when the plugin is removed (not just deactivated but actively deleted by the WordPress Admin).
+ *
+ * @package MimeTypes Link Icons
+ * @subpackage Uninstall
+ * @version 1.0
+ * @since 3.0
+ *
+ * @author Juliette Reinders Folmer
+ * @license http://creativecommons.org/licenses/GPL/2.0/ GNU General Public License, version 2
+ */
+
+if (!current_user_can('activate_plugins') || (!defined('ABSPATH') || !defined('WP_UNINSTALL_PLUGIN')))
+    exit();
+
+
+delete_option('mimetype_link_icon_options');
+delete_option('mimetype_link_icons_filesize_cache');

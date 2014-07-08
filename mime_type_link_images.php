dbac71ce37072532dd684d26c39f0ebf1776d39f
jrfnl <github_nospam@adviesenzo.nl>
2014-07-08 18:36:20 +0200

    Only initiate plugin when WP is not installing

--- mime_type_link_images.php
+++ mime_type_link_images.php
@@ -2157,7 +2157,9 @@ if ( ! class_exists( 'Mime_Types_Link_Icons' ) ) {
 
 
 	/* Instantiate our class */
-	add_action( 'plugins_loaded', 'mimetypes_link_icons_init' );
+	if ( ! defined( 'WP_INSTALLING' ) || WP_INSTALLING === false ) {
+		add_action( 'plugins_loaded', 'mimetypes_link_icons_init' );
+	}
 
 	if ( ! function_exists( 'mimetypes_link__icons_init' ) ) {
 		/**
